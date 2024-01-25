505b3f6be3e682ef583a5eb54558d2a011a46982
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-01-25 16:35:11 -0300

    botao de pesquisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-
+"use client"
+import React, { useState } from 'react';
 
 export default function Home() {
+  
+  const [nomeFilme, setNomeFilme] = useState('');
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setNomeFilme(event.target.value);
+  };
+
   return (
-   <main>
-      <h1 className="flex items-center justify-center h-screen">HOME</h1>
-   </main>
+<main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl mb-8">Consulte o seu filme favorito!</h1>
+
+      <div className="flex items-center">
+        <input
+          type="text"
+          placeholder="EX: baby driver"
+          value={nomeFilme}
+          onChange={handleChange}
+          className=" rounded-md border-gray-400 p-2 mr-2"
+        />
+        <button
+          className="bg-gray-600 text-black p-2 rounded-md"
+          onClick={() => console.log(nomeFilme)}
+        >
+          Procurar
+        </button>
+      </div>
+    </main>
   );
 }
