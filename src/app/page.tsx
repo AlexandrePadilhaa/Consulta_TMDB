ccd89722c10580239ada02af4287f0ef43dc77a2
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-01-26 10:37:24 -0300

    componente  topMovies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,16 @@
-"use client"
-import React, { useState } from 'react';
+import SearchButtom from "@/components/home/searchButtom";
+import TopMovies from "@/components/home/topMovies";
+
 
 export default function Home() {
-  
-  const [nomeFilme, setNomeFilme] = useState('');
-
-  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setNomeFilme(event.target.value);
-  };
 
   return (
-<main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl mb-8">Consulte o seu filme favorito!</h1>
+    <main className="flex flex-col items-center justify-center h-screen gap-20">
+      <h1 className=" md:text-4xl text-xl mb-8 ">Consulte o seu filme favorito!</h1>
 
-      <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="EX: baby driver"
-          value={nomeFilme}
-          onChange={handleChange}
-          className=" rounded-md border-gray-400 p-2 mr-2"
-        />
-        <button
-          className="bg-gray-600 text-black p-2 rounded-md"
-          onClick={() => console.log(nomeFilme)}
-        >
-          Procurar
-        </button>
-      </div>
+      <SearchButtom />
+      <TopMovies />
+   
     </main>
   );
 }
---
+++ src/components/home/searchButtom.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React, { useState } from 'react';
+
+function SearchButtom() {
+    const [movieName, setmovieName] = useState('');
+
+    const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setmovieName(event.target.value);
+    };
+
+    
+
+    return (
+
+        <div className="flex items-center ">
+            <input
+                type="text"
+                placeholder="EX: baby driver"
+                value={movieName}
+                onChange={handleChange}
+                className=" rounded-md border-gray-400 p-2 mr-2 w-44 md:w-72"
+            />
+            <button
+                className="bg-gray-600 text-black p-2 rounded-md"
+                onClick={() => console.log(movieName)}
+            >
+                Procurar
+            </button>
+        </div>
+    );
+
+}
+
+export default SearchButtom;
