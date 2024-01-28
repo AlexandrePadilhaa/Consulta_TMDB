228c4038351cf25c38bec8ed2b5126a711bb7e00
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-01-28 11:32:59 -0300

    rota dinamica para pagina detalhes filme

--- src/app/moviePage/[movieName]/page.tsx
+++ src/app/moviePage/[movieName]/page.tsx
@@ -4,15 +4,17 @@ import MovieDetails from "../movieDetails";
 export default function moviePage({ params }: {
     params: { movieName: string }
 }) {
-
+    const name = decodeURIComponent(params.movieName);
 
     return (
 
         <main className="flex flex-col items-center justify-center gap-20">
 
-            <h1 className="md:text-4xl text-xl mb-8 pt-20">Confira os detalhes sobre o filme!</h1>
-            <MovieDetails movieName={params.movieName} />
+            <h1 className="md:text-4xl text-xl mb-8 pt-20">Confira os detalhes sobre o filme! {name}</h1>
+            <MovieDetails movieName={name} />
+            
             <Link href="/">HOME</Link>
+
         </main>
 
     );
